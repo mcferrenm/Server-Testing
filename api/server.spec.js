aa25032380b0897f376d2b6ac8b83bc253fb9391
mcferrenm <mcferrenmax@gmail.com>
2019-03-07 16:56:38 -0500

    /api/users GET should return 200

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,9 +1,19 @@
-const request = require('supertest');
+const request = require("supertest");
 
-const server = require('./server.js');
+const server = require("./server.js");
 
-describe('server.js', () => {
-  it('should set testing environment', () => {
-    expect(process.env.DB_ENV).toBe('testing');
+describe("server.js", () => {
+  it("should set testing environment", () => {
+    expect(process.env.DB_ENV).toBe("testing");
+  });
+
+  describe("/api/users", () => {
+    describe("GET /", () => {
+      it("should return 200 status code", async () => {
+        const res = await request(server).get("/api/users");
+
+        expect(res.status).toBe(200);
+      });
+    });
   });
 });
--- users/users-router.js
+++ users/users-router.js
@@ -1,9 +1,9 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Users = require('./userModel.js');
+const Users = require("./userModel.js");
 
-router.get('/', async (req, res) => {
-  res.send('hi')
+router.get("/", async (req, res) => {
+  res.status(200).send("hi");
 });
 
 module.exports = router;
